8130cb6c4f543cb2ddc5b6b0e0ddc6ad28525530
Hye Mee Park <hyemee1021@gmail.com>
2024-02-12 10:02:39 +1100

    fixing bug

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = `http://localhost:8000/api/v1`;
+// export const BASE_URL = process.env.REACT_APP_ROOTAPI + `/api/v1`;
+export const BASE_URL = `https://tour-management-backend2.onrender.com/api/v1`;
